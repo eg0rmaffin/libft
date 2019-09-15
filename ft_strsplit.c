9f9f4726dfbcb7df2753dabf1ac882929c856c06
Caron Kumera <ckumera@mi-p3.21-school.ru>
2019-09-15 22:15:20 +0300

    strsplit WORKS! now u need to divide it

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,77 +12,77 @@
 
 #include "libft.h"
 
-int		ft_words_counter(char *s, char c)
-{
-	int words;
-	int run;
-
-	words = 0;
-	run = 0;
-	while (s[run])
-	{
-		while (s[run] == c)
-			run++;
-		if (s[run + 1] == c)
-			words++;
-		run++;
-	}
-	return (words);
-}
-
-size_t		ft_found_len(char *s, char c)
+size_t      ft_found_len(char *s, char c)
 {
 	size_t run;
 
 	run = 0;
-	while (s[run] != c)
+	while ((s[run] != c) && (s[run] != '\0'))
 		run++;
 	return (run);
 }
 
-
-char	**ft_strsplit(char const *s, char c)
+size_t     ft_words_counter(char *s, char c)
 {
-	char		**arr;
-	size_t		run;
-	size_t		i;
+	size_t run;
+	size_t words;
 
-	i = 0;
 	run = 0;
-	arr = (char**)malloc(sizeof(char*) * (ft_words_counter((char *)s, c) + 1));   //все ок точно ок
-	while (s[run])
+	words = 0;
+	while (s[run] != '\0')
 	{
-		while (s[run] == c)
+		if (s[run] == c)
 			run++;
 		if (s[run] != c)
 		{
-			arr[i] = ft_strsub(((char *)s + run), 0, ft_found_len((char *)s + run, c));
-			i++;
-			run = run + ft_found_len((char *)s + run, c) + 1;
+			run = run + ft_found_len((char*)&s[run], c) + 1;
+			words++;
+
 		}
 	}
-	return (arr);
+	return (words);
 }
 
+char		*ft_create_str(char *s, char c)
+{
+	size_t 	i;
+	char 	*str;
 
+	i = 0;
+	if (!(str = (char*)malloc(sizeof(char) * ft_found_len(s, c) + 1)))
+		return (NULL);
+	while ((s[i] != c) && (s[i] != '\0'))
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
 
+char    **ft_strsplit(char const *s, char c)
+{
+	char 	**arr;
+	size_t 	i;
+	size_t 	run;
 
-//int main() {
-//	char *s = "      split       this for   me  !       ";
-//	char **arr = ft_strsplit(s, ' ');
-
-//	for(int i = 0; i < ft_words_counter(s, ' '); i++)
-//	{
-//		free(arr[i]);
-//	}
-//	free(arr);
-
-//	printf("%s\n", arr[0]);
-//	printf("%s\n", arr[1]);
-//	printf("%s\n", arr[2]);
-//	printf("%s\n", arr[3]);
-//	     printf("%s\n", arr[4]);
-//	     printf("%s\n", arr[5]);
-//
-//	     printf("%d\n", ft_words_counter(s, ' '));
-//}
+	i = 0;
+	run = 0;
+	if (s == NULL)
+		return (NULL);
+	if(!(arr = (char**)malloc(sizeof(char*) * ft_words_counter((char*)s, c) + 1)))
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			i++;
+		if ((s[i] != c) && (s[i] != '\0'))
+		{
+			arr[run] = ft_create_str((char*)&s[i], c);
+			run++;
+			i = i + ft_found_len((char*)&s[i], c) + 1;
+		}
+	}
+	arr[run] = NULL;
+	return (arr);
+}
