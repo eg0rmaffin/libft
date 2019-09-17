3a6c55aabbeac5231af7b24e9fdb18f12657207e
Caron Kumera <ckumera@mi-q3.21-school.ru>
2019-09-17 23:44:27 +0300

    pls add memccpy

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,8 +17,11 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list	*rtrn;
 	t_list	*current;
 
+	if (lst == NULL)
+		return (NULL);
 	current = f(lst);
 	rtrn = current;
+	lst = lst->next;
 	while (lst)
 	{
 		current->next = f(lst);
